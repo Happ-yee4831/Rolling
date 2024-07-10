7d522e9d700db2e9f2d0f7603187a53a542f3683
hvrain <hvrain3222@naver.com>
2024-07-10 15:27:31 +0900

    initial commit

---
+++ src/AppRouter.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './layouts/Layout';
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="list" element={<List />} />
+          <Route path="post">
+            <Route index element={<Post />} />
+            <Route path=":id">
+              <Route index element={<PostId />} />
+              <Route path="edit" element={<PostIdEdit />} />
+              <Rouet path="message" element={<PostIdMessage />} />
+            </Route>
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
