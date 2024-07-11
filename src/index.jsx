2ff05a5b285fb00dec84099dc87a06f1f5fa154e
hvrain <hvrain3222@naver.com>
2024-07-11 16:54:03 +0900

    Header 완성
    Header.module.scss에 스타일 완성
    기능은 Link 밖에 없어서 Link만 적용함

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AppRouter from 'routers/AppRouter';
+import 'styles/reset.css';
+import 'styles/global.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<AppRouter />);
