c42ccbff6e34c14f817870f45822ffd65f788023
ayoooyh <127219927+ayoooyh@users.noreply.github.com>
2024-07-12 01:04:03 +0900

    Home point.01 text 구현 및 사진 미완성

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import './Home.css';
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="MainContainer">
+      <div className="MainFirstContainer">
+        <div>
+          <span className="PointOne">
+            Point. 01 <br />
+          </span>
+          <span className="Bold-text">
+            누구나 손쉽게, 온라인 <br />
+            롤링 페이퍼를 만들 수 있어요
+            <br />
+          </span>
+          <span className="Plain-text">로그인 없이 자유롭게 만들어요.</span>
+        </div>
+        <img className="RorringList-1" src="/assets/images/RorringList_1.png" alt="Rorring" />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
