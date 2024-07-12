4872bad3af15a8b2e3f4a87eaf108b3a5f26c755
ayoooyh <127219927+ayoooyh@users.noreply.github.com>
2024-07-13 00:47:31 +0900

    구경해보기 button 생성

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -35,6 +35,11 @@ function Home() {
           <span className="Plain-text">롤링 페이퍼에 이모지를 추가할 수 있어요.</span>
         </div>
       </div>
+      <div className="buttonContainer">
+        <button className="button" type="button" aria-label="구경해보기">
+          구경해보기
+        </button>
+      </div>
     </div>
   );
 }
