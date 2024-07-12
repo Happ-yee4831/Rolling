ba0db34b1d84ab034b583bc5434d01d9e45e4bf0
ayoooyh <127219927+ayoooyh@users.noreply.github.com>
2024-07-12 23:40:52 +0900

    MainFirstContainer 완성

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Home.css';
+import rorringList1 from '../assets/images/RorringList_1.png';
 
 function Home() {
   return (
     <div className="MainContainer">
       <div className="MainFirstContainer">
-        <div>
+        <div className="section1">
           <span className="PointOne">
             Point. 01 <br />
           </span>
@@ -16,7 +17,7 @@ function Home() {
           </span>
           <span className="Plain-text">로그인 없이 자유롭게 만들어요.</span>
         </div>
-        <img className="RorringList-1" src="/assets/images/RorringList_1.png" alt="Rorring" />
+        <img className="RorringList-1" src={rorringList1} alt="Rorring" />
       </div>
     </div>
   );
