40c367d65de088ce82f135a91aa8fdd0448ba8e5
ayoooyh <127219927+ayoooyh@users.noreply.github.com>
2024-07-13 00:08:03 +0900

    MainSecondContainer 완성

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './Home.css';
 import rorringList1 from '../assets/images/RorringList_1.png';
+import rorringList2 from '../assets/images/RorringList_2.png';
 
 function Home() {
   return (
     <div className="MainContainer">
       <div className="MainFirstContainer">
         <div className="section1">
-          <span className="PointOne">
+          <span className="Point">
             Point. 01 <br />
           </span>
           <span className="Bold-text">
@@ -17,7 +18,22 @@ function Home() {
           </span>
           <span className="Plain-text">로그인 없이 자유롭게 만들어요.</span>
         </div>
-        <img className="RorringList-1" src={rorringList1} alt="Rorring" />
+        <img src={rorringList1} alt="Rorring1" />
+      </div>
+
+      <div className="MainSecondContainer">
+        <img src={rorringList2} alt="Rorring2" />
+        <div className="section2">
+          <span className="Point">
+            Point. 02 <br />
+          </span>
+          <span className="Bold-text">
+            서로에게 이모지로 감정을 <br />
+            표현해보세요
+            <br />
+          </span>
+          <span className="Plain-text">롤링 페이퍼에 이모지를 추가할 수 있어요.</span>
+        </div>
       </div>
     </div>
   );
