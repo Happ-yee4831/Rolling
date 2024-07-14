e0fc1c930a1c3672bb6c486383574d6762424149
ayoooyh <127219927+ayoooyh@users.noreply.github.com>
2024-07-14 13:26:47 +0900

    기본 레이아웃 구현

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,19 @@ import React from 'react';
 import './Home.css';
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12">row1</div>
+      </div>
+      <div className="row">
+        <div className="col-12">row2</div>
+      </div>
+      <div className="row">
+        <div className="col-12">row2</div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
