e4da3b713cb53530bfa6dd9827ccbd7f9ceb1f55
hvrain <hvrain3222@naver.com>
2024-07-15 21:55:25 +0900

    Add: styled-components ThemeProvider and theme.js

--- src/components/SendersProfile.jsx
+++ src/components/SendersProfile.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import { Profile, RecentSenders, Sender, TotalSenders } from 'styles/styled/PostId';
 
+const Wrapper = styled.div`
+  position: relative;
+  &::after {
+    position: absolute;
+    border: 1px solid;
+    right: -28px;
+    height: 80%;
+  }
+`;
+
 function SendersProfile({ messages, count }) {
   return (
-    <>
+    <Wrapper>
       <RecentSenders>
         {messages?.map(message => (
           <Sender key={message.id + message.sender}>
@@ -16,7 +26,7 @@ function SendersProfile({ messages, count }) {
         {count}
         <span>명이 작성했어요!</span>
       </TotalSenders>
-    </>
+    </Wrapper>
   );
 }
 
--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -7,25 +7,29 @@ import Post from 'pages/Post';
 import PostId from 'pages/PostId';
 import PostIdEdit from 'pages/PostIdEdit';
 import PostIdMessage from 'pages/PostIdMessage';
+import { ThemeProvider } from 'styled-components';
+import theme from 'styles/styled/theme';
 
 function Router() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="list" element={<List />} />
-          <Route path="post">
-            <Route index element={<Post />} />
-            <Route path=":id">
-              <Route index element={<PostId />} />
-              <Route path="edit" element={<PostIdEdit />} />
-              <Route path="message" element={<PostIdMessage />} />
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="list" element={<List />} />
+            <Route path="post">
+              <Route index element={<Post />} />
+              <Route path=":id">
+                <Route index element={<PostId />} />
+                <Route path="edit" element={<PostIdEdit />} />
+                <Route path="message" element={<PostIdMessage />} />
+              </Route>
             </Route>
           </Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
