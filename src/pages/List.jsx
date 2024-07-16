b2edfbde2ad9d9c1f9faff7eb8a1b2be21c6be90
gjrefa9139 <gjrefa9139@gmail.com>
2024-07-16 14:23:25 +0900

    eslintrc 오류 메시지 off 설정 임시 아이콘.png 파일 크기 변경

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,18 +1,19 @@
 import { Link } from 'react-router-dom';
 import PaperList from 'components/PaperList';
 import style from 'styles/ListPage.module.css';
+import items from 'api/dummy/mock.json';
 
 function ListPage() {
   return (
     <>
       <section className={style.popularPaper}>
         <h2>인기 롤링 페이퍼 🔥</h2>
-        <PaperList />
+        <PaperList items={items} />
       </section>
 
       <section className={style.recentlyCreated}>
         <h2>최근에 만든 롤링 페이퍼 ⭐</h2>
-        <PaperList />
+        <PaperList items={items} />
       </section>
 
       <Link to="/post" className={style.postLink}>
