6b11f1fbcb21a6aa95d3a3ead9cede8eeecf3132
ayoooyh <127219927+ayoooyh@users.noreply.github.com>
2024-07-16 15:54:26 +0900

    Revert "row1 부트스트랩으로 변경"
    This reverts commit 25e4daf97aa7bd20a85dfbb2654aad5e4ab369ba.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,23 +7,16 @@ function Home() {
     <div className="container">
       <div className="row row-content mt-60">
         <div className="col">
-          <div className="row mb-16">
-            <div className="col">
-              <span className="content-point">Point. 01</span>
-            </div>
-          </div>
-          <div className="row mb-8">
-            <div className="col">
-              <span className="bold-text">
-                누구나 손쉽게, 온라인 <br />
-                롤링 페이퍼를 만들 수 있어요
-              </span>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col">
-              <span className="plain-text">로그인 없이 자유롭게 만들어요.</span>
-            </div>
+          <div className="section1">
+            <span className="Point">
+              Point. 01 <br />
+            </span>
+            <span className="Bold-text">
+              누구나 손쉽게, 온라인 <br />
+              롤링 페이퍼를 만들 수 있어요
+              <br />
+            </span>
+            <span className="Plain-text">로그인 없이 자유롭게 만들어요.</span>
           </div>
         </div>
         <div className="col">
