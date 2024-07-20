8842848b33cacfd90197afe4a399513ad2bca40b
강효성 <kds0001114@naver.com>
2024-07-20 23:43:15 +0900

    PostIdMessage 파일에서 사용할 api 관련 함수 구현

--- src/api/someApi.js
+++ src/api/someApi.js
@@ -34,8 +34,25 @@ const fetchProfileImg = async () => {
   return data;
 };
 
+const createMessage = async (messageData) => {
+  const response = await fetch(
+    `${BASE_URL}8-5/recipients/${messageData.recipientId}/messages/`,
+    {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(messageData),
+    },
+  );
+  if (!response.ok) {
+    throw new Error('메세지를 생성하는 데 실패했습니다.');
+  }
+  const body = await response.json();
+  return body;
+};
+
 export {
   getDataBackgroundImg,
   postUserData,
   fetchProfileImg,
+  createMessage,
 };
