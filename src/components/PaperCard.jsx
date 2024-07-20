997a6e70235449237af92bfad24a8c9c67f7b647
gjrefa9139 <gjrefa9139@gmail.com>
2024-07-20 18:33:30 +0900

    컴포넌트 PaperCard 작성 및 API 값들 할당
    중간 커밋을 못 해서 양이 많습니다.
    전체적인 레이아웃, 최근 메세지 섹션, 리엑션 섹션, API 값 할당 정도가 큰 틀입니다.
    
    Paper 스타일드 컴포넌트는 기본적인 css값,
    API에서 받아오는 값 중
    PaperCard에서 쓰이는 뒷 배경 이미지 또는 색상 값을 넣어주기 위해 사용했습니다.
    
    ※ 컴포넌트 분리 생각 중

--- src/components/PaperCard.jsx
+++ src/components/PaperCard.jsx
@@ -1,28 +1,106 @@
+import { Link } from 'react-router-dom';
 import style from 'styles/PaperCard.module.css';
-import profileIcon from 'assets/images/test_김강우.png';
-import emoji1 from 'assets/images/temporary_emoji_1.png';
-import emoji2 from 'assets/images/temporary_emoji_2.png';
-import emoji3 from 'assets/images/temporary_emoji_3.png';
+import styled from 'styled-components';
+import purple from 'assets/images/card-pattern-purple.png';
+import beige from 'assets/images/card-pattern-beige.png';
+import blue from 'assets/images/card-pattern-blue.png';
+import green from 'assets/images/card-pattern-green.png';
 
-function PaperCard({ item }) {
+const PATTERN = {
+  purple: { purple },
+  beige: { beige },
+  blue: { blue },
+  green: { green },
+};
+
+const Paper = styled.article`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 208px;
+  height: 232px;
+  padding: 30px 22px 20px 24px;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  border-radius: 16px;
+  box-shadow: 0px 2px 12px 0px rgba(0, 0, 0, 0.08);
+
+  @media (min-width: 768px) {
+    width: 275px;
+    height: 260px;
+    padding-right: 24px;
+  }
+
+  background-position: center;
+  background-repeat: no-repeat;
+
+  ${({ $paper }) =>
+    $paper.backgroundImageURL
+      ? `
+      background-image:
+      linear-gradient(rgba(0, 0, 0, 0.54), rgba(0, 0, 0, 0.54)),
+      url(${$paper.backgroundImageURL});
+      background-size: 100% 100%;
+      color: var(--white);
+
+      p {
+        color: var(--gray200);
+      }`
+      : `
+      background-color: var(--${$paper.backgroundColor}200);
+      background-image: url(${Object.values(PATTERN[$paper.backgroundColor])});
+      background-size: 50% 50%;
+      background-position : right bottom;
+      color: var(--gray900);
+
+      p {
+        color: var(--gray700);
+      }`};
+`;
+
+function PaperCard({ paper }) {
   return (
-    <div className={style.rollingPaper}>
-      <div className={style.writer}>
-        To. Sowon
-        <br />
-        {item.name}
-        <img src={profileIcon} alt="프로필 아이콘" width={81} height={28} />
-        <p>
-          30<span>명이 작성했어요!</span>
-        </p>
-      </div>
+    <Link to={`/items/${paper.id}`}>
+      <Paper $paper={paper}>
+        <section className={style.Author}>
+          <h3>To. {paper.name}</h3>
 
-      <div className={style.emoji}>
-        <img src={emoji1} alt="이모지 아이콘" />
-        <img src={emoji2} alt="이모지 아이콘" />
-        <img src={emoji3} alt="이모지 아이콘" />
-      </div>
-    </div>
+          <section className={style.recentMessages}>
+            {!!paper.messageCount &&
+              paper.recentMessages.map((writer, index) => (
+                <img
+                  key={writer.id}
+                  src={writer.profileImageURL}
+                  alt="프로필 아이콘"
+                  width={28}
+                  height={28}
+                  style={{
+                    borderRadius: '50px',
+                    border: '1.5px solid #ffffff',
+                    position: 'absolute',
+                    left: `${index * 16}px`,
+                  }}
+                />
+              ))}
+            {paper.messageCount > 3 ? <small>+{paper.messageCount - 3}</small> : ''}
+          </section>
+
+          <p>
+            {paper.messageCount}
+            <span>명이 작성했어요!</span>
+          </p>
+        </section>
+
+        <section className={style.topReactions}>
+          {!!paper.reactionCount &&
+            paper.topReactions.map(Reaction => (
+              <div key={Reaction.id} className={style.emoji}>
+                <div>{Reaction.emoji}</div>
+                <p>{Reaction.count}</p>
+              </div>
+            ))}
+        </section>
+      </Paper>
+    </Link>
   );
 }
 
