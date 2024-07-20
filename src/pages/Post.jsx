6cbaf52031fe61ed783c2a4e4cb57784b7fc6e19
강효성 <kds0001114@naver.com>
2024-07-20 21:58:57 +0900

    필요한 데이터 불러오고 보내는 기능 구현

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,7 +1,67 @@
-import React from 'react';
+/* eslint-disable */
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import {
+  SubmitForm,
+  TopContainer,
+  ToNameInput,
+  BottomContainer
+} from '../styles/PostStyle';
+import ColorAndImageButton from '../components/PostPageComponents/ColorAndImageButton';
+import BackgroundThingsList from '../components/PostPageComponents/BackgroundThingsList';
+import CreateButton from '../components/PostPageComponents/CreateButton';
+import { getDataBackgroundImg, postUserData } from '../api/someApi';
 
-function Post() {
-  return <div>Post</div>;
-}
+const Post = () => {
+  const [isColor, setIsColor] = useState(true);
+  const [userName, setUserName] = useState('');
+  const [error, setError] = useState('');
+  const [backgroundImgs, setBackgroundImgs] = useState(null);
+  const [selectImg, setSelectImg] = useState(null);
+  const [selectColor, setSelectColor] = useState('beige');
+  const navigate = useNavigate();
 
-export default Post;
+  const loadBackgroundImgData = async () => {
+    const { imageUrls } = await getDataBackgroundImg();
+    setBackgroundImgs(imageUrls);
+  };
+
+  useEffect(() => {
+    loadBackgroundImgData();
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log('handleSubmit called');
+
+    let sendData;
+    if (isColor) {
+      sendData = {
+        team: '8-5',
+        name: userName,
+        backgroundColor: selectColor,
+      };
+    } else {
+      sendData = {
+        team: '8-5',
+        name: userName,
+        backgroundColor: selectColor,
+        backgroundImageURL: selectImg,
+      };
+    }
+
+    const { id } = await postUserData(sendData);
+    navigate(`/post/${id}`);
+  };
+
+  const NameValueChange = (e) => {
+    setUserName(e.target.value);
+  };
+
+  const handleBlur = () => {
+    if (!userName) {
+      setError('값을 입력해 주세요.');
+    }
+  };
+
+  const isButtonDisabled = !userName;
