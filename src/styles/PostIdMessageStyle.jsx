73035a93fc93092fe2ab2d710dd10e8c551007e5
강효성 <kds0001114@naver.com>
2024-07-20 23:38:43 +0900

    TextContentBox에 적용시킬 스타일 구현

--- src/styles/PostIdMessageStyle.jsx
+++ src/styles/PostIdMessageStyle.jsx
@@ -114,6 +114,82 @@ const StyledSection = styled.section`
     height: 80px;
     border-radius: 100px;
   }
+
+  & .quill {
+    width: 720px;
+    @media (max-width: 767px) {
+      width: 320px;
+    }
+  }
+
+  & .ql-toolbar {
+    display: flex;
+    align-items: center;
+    padding: 12px 15px;
+    width: 100%;
+    height: 49px;
+    border-radius: 8px 8px 0px 0px;
+    background: #eeeeee;
+    gap: 14px;
+    @media (max-width: 767px) {
+      gap: 2px;
+      justify-content: center;
+    }
+
+    & .ql-formats {
+      display: flex;
+      padding: 0;
+      margin: 0;
+      gap: 2px;
+    }
+  }
+
+  & .ql-container {
+    width: 100%;
+    height: 211px;
+    padding: 16px;
+    border-radius: 0px 0px 8px 8px;
+    resize: none;
+
+    & .ql-editor {
+      padding: 0;
+      font-size: 16px;
+      & strong {
+        font-weight: 700;
+      }
+      & em {
+        font-style: italic;
+      }
+      & ul {
+        position: static;
+        width: inherit;
+        padding-top: 0;
+        padding-bottom: 0;
+        border: none;
+        border-radius: none;
+        background-color: #ffffff;
+        box-shadow: none;
+        z-index: 0;
+      }
+
+      & li {
+        padding: 0;
+        color: inherit;
+        line-height: normal;
+      }
+
+      & li:hover {
+        background-color: inherit;
+      }
+    }
+
+    @media (max-width: 767px) {
+      & input,
+      & button {
+        width: 320px;
+      }
+    }
+  }
 `;
 
 const ProfileImgList = styled.div`
