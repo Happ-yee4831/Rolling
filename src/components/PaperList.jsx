eeba95fdcf66ff948938ac2fe3fee5fa23891182
gjrefa9139 <gjrefa9139@gmail.com>
2024-07-20 18:24:35 +0900

    컴포넌트 - PaperList 추가 및 API 연동
    API에서 다른 목록을 불러오는 주소값을 줘서
    화면 너비에 따라 받아오는 목록 개수에 제한만 주었습니다.

---
+++ src/components/PaperList.jsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from 'react';
+import style from 'styles/PaperList.module.css';
+import getRecipients from 'api/dummy/listPageTestApi';
+import PaperCard from './PaperCard';
+import ChangeListButton from './ChangeListButton';
+
+const RECIPIEMTS_KEY = {
+  count: 0,
+  next: null,
+  previous: null,
+  results: [],
+};
+
+const getPaperNum = () => {
+  const width = window.innerWidth;
+  const PC_SIZE = 1248;
+
+  return width < PC_SIZE ? 8 : 4;
+};
+
+function PaperList() {
+  const [list, setList] = useState(RECIPIEMTS_KEY);
+  const [paperNum, setPaperNum] = useState(getPaperNum());
+
+  const fetchSortedData = async param => {
+    setList(await getRecipients(param));
+  };
+
+  useEffect(() => {
+    const handlePaperRecount = () => {
+      setPaperNum(getPaperNum());
+    };
+
+    window.addEventListener('resize', handlePaperRecount);
+    fetchSortedData(`?limit=${paperNum}`);
+
+    return () => {
+      window.removeEventListener('resize', handlePaperRecount);
+    };
+  }, [paperNum]);
+
+  const handleListChange = changeList => {
+    fetchSortedData(changeList);
+  };
+
+  return (
+    <div className={style.buttonPosition}>
+      <div className={style.paperList}>
+        {list.results.map(paper => (
+          <PaperCard paper={paper} key={paper.id} />
+        ))}
+      </div>
+
+      <ChangeListButton prevList={list.previous} nextList={list.next} onChange={handleListChange} />
+    </div>
+  );
+}
+
+export default PaperList;
