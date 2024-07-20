11959c6a4ef05a576c69ac724d0950f9d828c940
강효성 <kds0001114@naver.com>
2024-07-20 21:40:01 +0900

    생성하기 버튼 구현

---
+++ src/components/PostPageComponents/CreateButton.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable */
+
+
+const CreateButton = ({ disabled }) => {
+  return <CreateButtonStyle type="submit" disabled={disabled}>생성하기</CreateButtonStyle>;
+}
+
+export default CreateButton;
