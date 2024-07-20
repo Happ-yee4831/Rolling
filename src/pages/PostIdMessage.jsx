6e8a9ac01b7d1193d29cf7c545caec84dba36618
강효성 <kds0001114@naver.com>
2024-07-21 00:18:56 +0900

    입력한 데이터를 전송하는 기능 구현

--- src/pages/PostIdMessage.jsx
+++ src/pages/PostIdMessage.jsx
@@ -1,7 +1,47 @@
-import React from 'react';
+/* eslint-disable */
+import { useNavigate, useParams } from 'react-router-dom';
+import { useState } from 'react';
 
-function PostIdMessage() {
-  return <div>PostIdMessage</div>;
+import FromNameInput from '../components/PostIdMessagePageComponents/FromNameInput';
+import ProfileImgInputSelector from '../components/PostIdMessagePageComponents/ProfileImgInputSelector';
+import TextContentBox from '../components/PostIdMessagePageComponents/TextContentBox';
+import RelationshipAndFontSelector from '../components/PostIdMessagePageComponents/RelationshipAndFontSelector';
+import { createMessage } from '../api/someApi';
+
+const PostIdMessage = () => {
+  const [recipientId, setRecipientId] = useState('');
+  const [sender, setSender] = useState('');
+  const [profileImageURL, setProfileImageURL] = useState(
+    'https://ibb.co/cvKLyF5',
+  );
+  const [relationship, setRelationship] = useState('');
+  const [content, setContent] = useState('');
+  const [font, setFont] = useState('');
+  const navigate = useNavigate();
+
+  const { id } = useParams();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const messageData = {
+      team: '8-5',
+      recipientId: id,
+      sender,
+      profileImageURL,
+      relationship,
+      content,
+      font,
+    };
+
+    await createMessage(messageData);
+    navigate(`/post/${id}`);
+  };
+
+  const isButtonDisabled = !sender || !content;
+
+  return (
+
+  );
 }
 
 export default PostIdMessage;
