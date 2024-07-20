98d5d74e52e00c17ed16d2287b94c9ea077064c9
gjrefa9139 <gjrefa9139@gmail.com>
2024-07-20 18:20:13 +0900

    코드 위치 변경 (ListPage -> PaperList)

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,26 +1,17 @@
-import PaperCard from 'components/PaperCard';
 import style from 'styles/ListPage.module.css';
-import items from 'api/dummy/mock.json';
+import PaperList from 'components/PaperList';
 
 function ListPage() {
   return (
     <main>
       <section className={style.popularPaper}>
         <h2>인기 롤링 페이퍼 🔥</h2>
-        <div className={style.paperList}>
-          {items.map(item => (
-            <PaperCard item={item} key={item.id} />
-          ))}
-        </div>
+        <PaperList />
       </section>
 
       <section className={style.recentlyCreated}>
         <h2>최근에 만든 롤링 페이퍼 ⭐</h2>
-        <div className={style.paperList}>
-          {items.map(item => (
-            <PaperCard item={item} key={item.id} />
-          ))}
-        </div>
+        <PaperList />
       </section>
     </main>
   );
