dada2686b431aef54ace1e988285b9b91491273b
강효성 <kds0001114@naver.com>
2024-07-20 21:22:56 +0900

    Post페이지 컴포넌트 파일과 스타일드 컴포넌트 파일 생성 후 클릭에 따라 스타일이 변하는 컬러 버튼 이미지 버튼 기능 구현

---
+++ src/components/PostPageComponents/ColorAndImageButton.jsx
@@ -0,0 +1,29 @@
+
+
+
+const ColorAndImageButton = ({ setIsColor, isColor }) => {
+  const changeType = (type) => {
+    setIsColor(type);
+  };
+
+  return (
+    <UpperLowerInterval>
+      {isColor === true ? (
+        <SelectedButton type="button">컬러</SelectedButton>
+      ) : (
+        <UnselectedButton type="button" onClick={() => changeType(true)}>
+          컬러
+        </UnselectedButton>
+      )}
+      {isColor === false ? (
+        <SelectedButton type="button">이미지</SelectedButton>
+      ) : (
+        <UnselectedButton type="button" onClick={() => changeType(false)}>
+          이미지
+        </UnselectedButton>
+      )}
+    </UpperLowerInterval>
+  );
+};
+
+export default ColorAndImageButton;
