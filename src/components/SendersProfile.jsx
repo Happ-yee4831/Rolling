e54801483bdf341fa6611afa21caacde60620673
hvrain <hvrain3222@naver.com>
2024-07-23 20:24:21 +0900

    Fix: SendersProfile count error

--- src/components/SendersProfile.jsx
+++ src/components/SendersProfile.jsx
@@ -11,9 +11,9 @@ function SendersProfile({ messages, count }) {
               <S.Profile src={message.profileImageURL} alt="recent messages profile" />
             </S.Sender>
           ))}
-          {count && count > 3 && <S.Sender>+{count - 3}</S.Sender>}
+          {count && count !== 0 && count > 3 && <S.Sender>+{count - 3}</S.Sender>}
         </S.RecentSenders>
-        {count && (
+        {count && count !== 0 && (
           <S.TotalSenders>
             {count}
             <span>명이 작성했어요!</span>
